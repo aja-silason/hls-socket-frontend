26bd9f3a573b937531b083966f8f4a9a7a2c06e4
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-04-28 17:30:28 +0100

    feat: send thing

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,8 +1,9 @@
 export function HomePage(){
 
     return (
-        <div className="flex items-center border justify-center w-screen">
+        <div className="flex items-center justify-center w-screen h-screen">
             <p className="font-semibold">This is my home</p>
+            <video id="local-video" controls autoPlay muted ></video>
         </div>
     )
 } 
